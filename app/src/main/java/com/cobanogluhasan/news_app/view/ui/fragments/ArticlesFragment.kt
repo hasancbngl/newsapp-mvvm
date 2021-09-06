3b9dbdf672549f650c982651052a21b0d7a6deda
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2021-09-06 11:44:27 +0300

    implement save and delete from db

--- app/src/main/java/com/cobanogluhasan/news_app/view/ui/fragments/ArticlesFragment.kt
+++ app/src/main/java/com/cobanogluhasan/news_app/view/ui/fragments/ArticlesFragment.kt
@@ -6,13 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
+import com.cobanogluhasan.news_app.data.viewmodel.NewsViewModel
 import com.cobanogluhasan.news_app.databinding.FragmentArticleBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ArticlesFragment() : Fragment() {
 
     private lateinit var binding: FragmentArticleBinding
     private val args : ArticlesFragmentArgs by navArgs()
+    private val viewModel : NewsViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,6 +34,11 @@ class ArticlesFragment() : Fragment() {
         binding.webView.apply {
             webViewClient = WebViewClient()
             loadUrl(article.url)
+        }
+
+        binding.fab.setOnClickListener{
+            viewModel.saveArticle(article)
+            Snackbar.make(view, "Article saved!", Snackbar.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/cobanogluhasan/news_app/view/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/cobanogluhasan/news_app/view/ui/fragments/SavedNewsFragment.kt
@@ -4,19 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.accessibility.AccessibilityEventCompat.setAction
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cobanogluhasan.news_app.data.viewmodel.NewsViewModel
 import com.cobanogluhasan.news_app.databinding.FragmentSavedNewsBinding
 import com.cobanogluhasan.news_app.view.adapters.NewsAdapter
+import com.google.android.material.snackbar.Snackbar
 
 class SavedNewsFragment() : Fragment() {
 
     private lateinit var binding: FragmentSavedNewsBinding
-    private val viewModel : NewsViewModel by activityViewModels()
-    private lateinit var newsAdapter : NewsAdapter
+    private val viewModel: NewsViewModel by activityViewModels()
+    private lateinit var newsAdapter: NewsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +39,35 @@ class SavedNewsFragment() : Fragment() {
             val action = SavedNewsFragmentDirections.actionSavedNewsFragmentToArticlesFragment(it)
             findNavController().navigate(action)
         }
+        viewModel.getSavedNews().observe(viewLifecycleOwner, {
+            newsAdapter.differ.submitList(it)
+        })
+
+        initSwipeToDelete()
+    }
+
+    private fun initSwipeToDelete() {
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean = true
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val article = newsAdapter.differ.currentList[position]
+                viewModel.deleteArticle(article)
+                Snackbar.make(requireView(), "Article deleted!", Snackbar.LENGTH_SHORT).apply {
+                    setAction("Undo") { viewModel.saveArticle(article) }
+                }.show()
+            }
+        }
+
+        ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(binding.rvSavedNews)
     }
 
     private fun initRecycler() {
